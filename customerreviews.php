1a70fb239661a4d25cdba2df51c33d9c88e659f4
Patryk Tchórzewski <patryk@dark-side.pro>
2019-02-18 18:22:57 +0100

    initial commit

---
+++ customerreviews.php
@@ -0,0 +1,336 @@
+<?php
+/**
+* 2007-2019 PrestaShop.
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2019 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Customerreviews extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'customerreviews';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Dark-Side.pro';
+        $this->need_instance = 1;
+
+        /*
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Customer Reviews');
+        $this->description = $this->l('Now customer can add reviews after buy product. ');
+
+        $this->confirmUninstall = $this->l('');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    private function createTab()
+    {
+        $response = true;
+        $parentTabID = Tab::getIdFromClassName('AdminDarkSideMenu');
+        if ($parentTabID) {
+            $parentTab = new Tab($parentTabID);
+        } else {
+            $parentTab = new Tab();
+            $parentTab->active = 1;
+            $parentTab->name = array();
+            $parentTab->class_name = 'AdminDarkSideMenu';
+            foreach (Language::getLanguages() as $lang) {
+                $parentTab->name[$lang['id_lang']] = 'Dark-Side.pro';
+            }
+            $parentTab->id_parent = 0;
+            $parentTab->module = '';
+            $response &= $parentTab->add();
+        }
+        $parentTab_2ID = Tab::getIdFromClassName('AdminDarkSideMenuSecond');
+        if ($parentTab_2ID) {
+            $parentTab_2 = new Tab($parentTab_2ID);
+        } else {
+            $parentTab_2 = new Tab();
+            $parentTab_2->active = 1;
+            $parentTab_2->name = array();
+            $parentTab_2->class_name = 'AdminDarkSideMenuSecond';
+            foreach (Language::getLanguages() as $lang) {
+                $parentTab_2->name[$lang['id_lang']] = 'Dark-Side Config';
+            }
+            $parentTab_2->id_parent = $parentTab->id;
+            $parentTab_2->module = '';
+            $response &= $parentTab_2->add();
+        }
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = 'AdministratorFacebookChat';
+        $tab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $tab->name[$lang['id_lang']] = 'Customer reviews';
+        }
+        $tab->id_parent = $parentTab_2->id;
+        $tab->module = $this->name;
+        $response &= $tab->add();
+
+        return $response;
+    }
+
+    private function tabRem()
+    {
+        $id_tab = Tab::getIdFromClassName('AdministratorFacebookChat');
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            $tab->delete();
+        }
+        $parentTab_2ID = Tab::getIdFromClassName('AdminDarkSideMenuSecond');
+        if ($parentTab_2ID) {
+            $tabCount_2 = Tab::getNbTabs($parentTab_2ID);
+            if ($tabCount_2 == 0) {
+                $parentTab_2 = new Tab($parentTab_2ID);
+                $parentTab_2->delete();
+            }
+        }
+        $parentTabID = Tab::getIdFromClassName('AdminDarkSideMenu');
+        if ($parentTabID) {
+            $tabCount = Tab::getNbTabs($parentTabID);
+            if ($tabCount == 0) {
+                $parentTab = new Tab($parentTabID);
+                $parentTab->delete();
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update.
+     */
+    public function install()
+    {
+        Configuration::updateValue('CUSTOMERREVIEWS_LIVE_MODE', false);
+
+        $this->createTab();
+
+        include dirname(__FILE__).'/sql/install.php';
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('displayProductTab') &&
+            $this->registerHook('displayHome');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('CUSTOMERREVIEWS_LIVE_MODE');
+
+        $this->tabRem();
+
+        include dirname(__FILE__).'/sql/uninstall.php';
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form.
+     */
+    public function getContent()
+    {
+        /*
+         * If values have been submitted in the form, process.
+         */
+        if (((bool) Tools::isSubmit('submitCustomerreviewsModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitCustomerreviewsModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'CUSTOMERREVIEWS_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled'),
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled'),
+                            ),
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter a valid email address'),
+                        'name' => 'CUSTOMERREVIEWS_ACCOUNT_EMAIL',
+                        'label' => $this->l('Email'),
+                    ),
+                    array(
+                        'type' => 'password',
+                        'name' => 'CUSTOMERREVIEWS_ACCOUNT_PASSWORD',
+                        'label' => $this->l('Password'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    protected function approveComments()
+    {
+    }
+
+    protected function deleteComment()
+    {
+    }
+
+    protected function getAllComments()
+    {
+    }
+
+    protected function getSliderComments()
+    {
+    }
+
+    protected function getProductComments()
+    {
+    }
+
+    protected function addProductComment()
+    {
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'CUSTOMERREVIEWS_LIVE_MODE' => Configuration::get('CUSTOMERREVIEWS_LIVE_MODE', true),
+            'CUSTOMERREVIEWS_ACCOUNT_EMAIL' => Configuration::get('CUSTOMERREVIEWS_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'CUSTOMERREVIEWS_ACCOUNT_PASSWORD' => Configuration::get('CUSTOMERREVIEWS_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+
+    public function hookDisplayProductTab()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookDisplayHome()
+    {
+    }
+}
